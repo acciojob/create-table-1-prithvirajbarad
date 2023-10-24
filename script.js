076a71ecb69393182844bb58ebd0bd6556c763e6
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-24 19:24:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function insert_Row() {
     //Write your code here
   
-  let table = document.getElementById("sampleTable")
-	let tr = document.createElement("tr")
-	let td1 = document.createElement("td")
-	let td2 = document.createElement("td")
-  td1.innerText = "New Cell1"
-	td2.innerText = "New Cell2"
-	tr.appendChild(td1)
-	tr.appendChild(td2)
-	table.prepend(tr)
-}
+   let table = document.getElementById("sampleTable");
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  let td2 = document.createElement("td");
+
+  // Clear the existing content of the cells, if any
+  td1.innerHTML = '';
+  td2.innerHTML = '';
+
+  // Set the new text for the cells
+  td1.innerText = "New Cell1";
+  td2.innerText = "New Cell2";
+
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  table.prepend(tr);
