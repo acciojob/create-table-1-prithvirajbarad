a795d0c82ee92ff28330f23ce4eeb7edd80502c9
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-25 09:53:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function insert_Row() {
     //Write your code here
-  
   let table = document.getElementById("sampleTable")
 	let tr = document.createElement("tr")
 	let td1 = document.createElement("td")
